4065366ff3583a763a9347be1d91889b2a5672b9
rajmahal4 <55266192+rajmahal4@users.noreply.github.com>
2020-04-04 16:00:42 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,93 +1,116 @@
-const screen = document.querySelector(".calculator-screen");
-const btns = document.querySelectorAll(".number");
-const equal = document.querySelector(".equal-sign")
-const op = document.querySelectorAll(".operator")
-const clr = document.querySelector(".all-clear");
-let a;
-let b;
-let operator;
-let toggle = true;
-add = (a, b) => a + b;
-subtract = (a, b) => a - b;
-divide = (a, b) => a / b;
-multiply = (a, b) => a * b;
+//The calculator object holds all the data that we need to construct a valid expression//
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
 
-function operate(a, b, operator) {
-    switch (operator) {
-        case "+":
-            return add(a, b);
-        case "-":
-            return subtract(a, b);
-        case "*":
-            return multiply(a, b);
-        case "/":
-            return divide(a, b);
+function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+    const { target } = event; // is equivalent to -> const target = event.target;
+    if (!target.matches("button")) {
+        //exit the function
+        return;
+    }
+    if (target.classList.contains('operator')) {
+        handleOperator(target.value);
+        console.log(target.value);
+        updateDisplay();
+        return;
     }
 
+    if (target.classList.contains('decimal')) {
+        inputDecimal(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('all-clear')) {
+        clearAll();
+        updateDisplay();
+        return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay();
+});
+
+
+function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+  
+    if (waitingForSecondOperand === true) {
+      calculator.displayValue = digit;
+      calculator.waitingForSecondOperand = false;
+    } else {
+      calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+  
+    console.log(calculator);
+  }
+
+  function inputDecimal(dot) {
+    if (calculator.waitingForSecondOperand === true) return;
+  
+    // If the `displayValue` does not contain a decimal point
+    if (!calculator.displayValue.includes(dot)) {
+      // Append the decimal point
+      calculator.displayValue += dot;
+    }
+  }
+
+function handleOperator(nextOperator) {
+  const { firstOperand, displayValue, operator } = calculator
+  const inputValue = parseFloat(displayValue);
+
+  if (operator && calculator.waitingForSecondOperand)  {
+    calculator.operator = nextOperator;
+    console.log(calculator);
+    return;
+  }
+
+  if (firstOperand == null) {
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const currentValue = firstOperand || 0;
+    const result = performCalculation[operator](currentValue, inputValue);
+
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+  console.log(calculator);
+}
+const performCalculation = {
+    '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+  
+    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+  
+    '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+  
+    '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+  
+    '=': (firstOperand, secondOperand) => secondOperand
+  };
+
+const clear = document.querySelector(".all-clear function");
+
+function clearAll(){
+    const { firstOperand, displayValue, operator, waitingForSecondOperand } = calculator;
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+    
 }
 
-function display() {
-    let displayValue = '';
-    btns.forEach(btn =>
-        btn.addEventListener("click", (e) => {
-            if(!toggle){
-                clear();
-            }
-                displayValue = screen.textContent;
-                screen.textContent += e.target.value;
-        }))
-}
-
-equal.addEventListener("click", () => {
-    let results = Array.from(screen.textContent).join("");
-    console.log(Array.from(screen.textContent).join(""));
-    if(results.includes("+")){
-    let a  = results.split("+")[0];
-    let b  = results.split("+")[1];
-    operator = "+";
-    results = operate(Number(a), Number(b), "+");
-    screen.textContent =  operate(Number(a), Number(b), "+");
-}
-    else if(results.includes("-")){
-    let a  = results.split("-")[0];
-    let b  = results.split("-")[1];
-    operator = "-";
-    results = operate(Number(a), Number(b), "-");
-    screen.textContent =  operate(Number(a), Number(b), "-");
-}
-    else if(results.includes("/")){
-    let a  = results.split("/")[0];
-    let b  = results.split("/")[1];
-    operator = "/";
-    results = operate(Number(a), Number(b), "/");
-    screen.textContent =  operate(Number(a), Number(b), "/");
-}
-    else if(results.includes("*")){
-    let a  = results.split("*")[0];
-    let b  = results.split("*")[1];
-    operator = "*";
-    results = operate(...`${a}`, ...`${b}`, "*");
-    screen.textContent =  operate(Number(a), Number(b), "*");
-}       
-        return results;
-})
-
-
-
-op.forEach(ops =>{
-    ops.addEventListener("click",(e) => {
-     operator = e.target.value;
-     screen.textContent += operator;
-     return operator;
-})
-})
-
-display();
-
-clr.addEventListener("click", clear);
-function clear() {
-    let a = 0;
-    let b = 0;
-    let operator = "";
-    screen.innerText = "";
-}
